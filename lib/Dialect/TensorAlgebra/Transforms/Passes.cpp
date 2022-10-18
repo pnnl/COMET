872d3e88ff36c6832800bd516ea4a782b5ace3a9
rizwan.ashraf <rizwan.ashraf@knights.ucf.edu>
2022-10-18 13:58:41 -0700

    first commit that performs analysis on ta-dialect to get info needed for hw-sw codesign experiments

--- lib/Dialect/TensorAlgebra/Transforms/Passes.cpp
+++ lib/Dialect/TensorAlgebra/Transforms/Passes.cpp
@@ -51,6 +51,7 @@
 #include <algorithm>
 #include <map>
 #include <set>
+#include <iostream>
 
 #include "mlir/Dialect/Linalg/EDSC/Intrinsics.h"
 #include "mlir/Dialect/Linalg/IR/LinalgOps.h"
@@ -127,6 +128,230 @@ namespace
 
 } // end anonymous namespace.
 
+namespace
+{
+  class SimulationAnalysisPass
+      : public mlir::PassWrapper<SimulationAnalysisPass, mlir::FunctionPass>
+  {
+
+  public:
+    void runOnFunction() final;
+
+    void SimulationAnalysis(tensorAlgebra::TensorMultOp op);
+  }; // class SimulationAnalysisPass
+} // End anonymous namespace
+
+// determine the information needed for hardware simulation analysis.
+std::tuple<std::map<int, long long>, std::vector<std::vector<int>>>
+DetermineIndexVals (Operation *rhsOp0, Operation *rhsOp1)
+{
+  std::map<int, long long> IndexToSizes; // i = 128; j = 128; k = 128;
+  std::vector<std::vector<int>> IndicesAll;  // input0 = {i, j}; input1 = {j, k}; output = {i, k}; 
+
+  std::vector<Value> all_lbls_value;
+  std::vector<Value> rhs0_lbls_value;
+  std::vector<Value> rhs1_lbls_value;
+
+  if (isa<tensorAlgebra::TensorMultOp>(rhsOp0))
+  {
+    auto numOperands = cast<tensorAlgebra::TensorMultOp>(rhsOp0).getNumOperands();  // get the upper-bound.
+    for (unsigned int i = 2; i < numOperands; i++) // the label info for output of TensorMultOp
+    {
+      auto lbl = cast<tensorAlgebra::TensorMultOp>(rhsOp0).getOperand(i);
+      // comet_vdump(lbl);
+      rhs0_lbls_value.push_back(lbl);
+      all_lbls_value.push_back(lbl);
+    }
+  } // end-mult-op
+  else if (isa<tensorAlgebra::DenseTensorDeclOp>(rhsOp0))
+  {
+    auto rhs0Labels = cast<tensorAlgebra::DenseTensorDeclOp>(rhsOp0).labels();
+    for (auto lbl : rhs0Labels)
+    {
+      rhs0_lbls_value.push_back(lbl);
+      all_lbls_value.push_back(lbl);
+      //comet_vdump(lbl);
+    }
+  }
+  else
+  {
+    assert (false && "TO BE SUPPORTED! (Sparse)");
+  }
+
+  if (isa<tensorAlgebra::DenseTensorDeclOp>(rhsOp1))
+  {
+    auto rhs1Labels = cast<tensorAlgebra::DenseTensorDeclOp>(rhsOp1).labels();
+    for (auto lbl : rhs1Labels)
+    {
+      rhs1_lbls_value.push_back(lbl);
+      auto result1 = std::find(all_lbls_value.begin(), all_lbls_value.end(), lbl);
+      if (result1 == all_lbls_value.end())
+      {
+        all_lbls_value.push_back(lbl);
+      }
+      // comet_vdump(lbl);
+    }
+  } // end-dense-decl
+  else
+  {
+    assert (false && "TO BE SUPPORTED! (Sparse)");
+  }
+
+  std::vector<int> rhs0_lbls;
+  std::vector<int> rhs1_lbls;
+  for (unsigned int i = 0; i < all_lbls_value.size(); i++)
+  {
+    auto result1 = std::find(rhs0_lbls_value.begin(), rhs0_lbls_value.end(), all_lbls_value[i]);
+    if (result1 != rhs0_lbls_value.end())
+    {
+      rhs0_lbls.push_back(i);
+    }
+
+    auto result2 = std::find(rhs1_lbls_value.begin(), rhs1_lbls_value.end(), all_lbls_value[i]);
+    if (result2 != rhs1_lbls_value.end())
+    {
+      rhs1_lbls.push_back(i);
+    }
+    
+    // populate map for output
+    auto indx = cast<tensorAlgebra::IndexLabelStaticOp>(all_lbls_value[i].getDefiningOp()).getOperand(1);  // get the upper-bound.
+    auto indxVal = cast<mlir::ConstantIndexOp>(indx.getDefiningOp()).getValue();
+    if (IndexToSizes.find(i) == IndexToSizes.end())
+    {
+      IndexToSizes[i] = indxVal;
+    }
+  }
+
+  std::vector<int> sum_lbls;
+  std::set_intersection(rhs0_lbls.begin(), rhs0_lbls.end(), rhs1_lbls.begin(), rhs1_lbls.end(), std::back_inserter(sum_lbls));
+  std::vector<int> all_lbls;
+  std::set_union(rhs0_lbls.begin(), rhs0_lbls.end(), rhs1_lbls.begin(), rhs1_lbls.end(), std::back_inserter(all_lbls));
+  std::vector<int> ret_lbls;
+  std::set_difference(all_lbls.begin(), all_lbls.end(), sum_lbls.begin(), sum_lbls.end(), std::back_inserter(ret_lbls));
+
+  
+  // populate index-labels for output
+  IndicesAll.push_back(rhs0_lbls);
+  IndicesAll.push_back(rhs1_lbls);
+  IndicesAll.push_back(ret_lbls);
+
+  return std::make_tuple(IndexToSizes, IndicesAll);
+}
+
+bool DetermineTensorType(Operation *op)
+{
+  bool result = false;  // default: sparse
+
+  if (isa<tensorAlgebra::DenseTensorDeclOp>(op)) 
+  {
+    //comet_debug() << "found dense\n";
+    result = true;
+  } 
+  else if (isa<tensorAlgebra::SparseTensorDeclOp>(op)) 
+  {
+    comet_debug() << "found sparse\n";
+  } 
+  else if (isa<tensorAlgebra::TensorMultOp>(op)) // chain operation
+  {
+    auto operands = op->getOperands();
+    Operation* rhsOp0 = operands[0].getDefiningOp();
+    Operation* rhsOp1 = operands[1].getDefiningOp();
+
+    bool MultOp0 = DetermineTensorType(rhsOp0);
+    bool MultOp1 = DetermineTensorType(rhsOp1);
+    if (MultOp0 && MultOp1)  // if any input is sparse, assume output is sparse
+      result = true;
+  }
+  else
+  {
+    comet_debug() << "ERROR: undetermined case in Simulation-Analysis Pass\n";
+  }
+
+  return result;
+}
+
+
+void SimulationAnalysisPass::SimulationAnalysis(tensorAlgebra::TensorMultOp op)
+{
+  comet_debug() << "Checking the following op: \n";
+  comet_pdump(op);
+
+  // determine the format of inputs
+  auto operands = op->getOperands();
+  Operation* rhsOp0 = operands[0].getDefiningOp();
+  Operation* rhsOp1 = operands[1].getDefiningOp();
+
+  comet_debug() << "\tFollowing are the two operands of op: \n";
+  comet_pdump(rhsOp0);
+  comet_pdump(rhsOp1);
+
+  // Determine whether the Tensor inputs are Dense or Sparse
+  bool IsformatDenseOp0 = DetermineTensorType (rhsOp0);
+  bool IsformatDenseOp1 = DetermineTensorType (rhsOp1);
+
+  if (!IsformatDenseOp0)
+    assert (false && "TO BE SUPPORTED! (Sparse)");
+
+  if (!IsformatDenseOp1)
+    assert (false && "TO BE SUPPORTED! (Sparse)");
+
+  // determine the indexing maps
+  std::map<int, long long> IndexToSizes; 
+  std::vector<std::vector<int>> IndicesAll;
+
+  std::tie(IndexToSizes, IndicesAll) = DetermineIndexVals (rhsOp0, rhsOp1);
+  
+  // print the output to std-out
+  std::vector<char> alphabets = {'A', 'B', 'C'};
+  std::vector<char> indexAlphabets = {'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p'};
+  assert(IndexToSizes.size() < indexAlphabets.size()  && "Sim-analysis pass: too many indices.\n");
+
+  unsigned i = 0;
+  comet_debug() << "starting config file...\n";
+  std::cout << "problem:" << "\n"; 
+  std::cout << "  shape:" << "\n"; 
+  std::cout << "    name: " << "\"tc\"" << "\n";
+  std::cout << "    dimensions: " << "[ ";
+  for (auto elem : IndexToSizes) 
+  {
+    if ((unsigned) elem.first < indexAlphabets.size()-1)
+    {
+      std::cout << indexAlphabets[elem.first] << ", ";
+    }
+    else
+    {
+      assert(false && "Sim-analysis pass: index out of bounds.\n");
+    }
+  }
+  std::cout << " ]" << "\n";
+
+  std::cout << "    data-spaces:" << "\n";
+  for (auto outer : IndicesAll) 
+  {
+    std::cout << "    - name: " << alphabets[i++] << "\n";
+    std::cout << "      projection:" << "\n";
+    for (auto inner : outer)
+    {
+      // making this element access in the indexAlphabets vector is safe
+      std::cout << "        - - " << "[" << indexAlphabets[inner] << "]" << "\n";
+    }
+
+    // the output
+    if (i == alphabets.size())
+      std::cout << "      read-write: True" << "\n";
+  }
+
+  std::cout << "  instance: {";
+  for (auto elem : IndexToSizes)
+  {
+    std::cout << indexAlphabets[elem.first] << ": " <<
+            elem.second << ", ";
+  }
+  std::cout << "}\n";
+  comet_debug() << "done with config file...\n";
+
+}
+
 void TAOptimalTCFactorizationPass::runOnFunction()
 {
   auto function = getFunction();
@@ -151,6 +376,17 @@ void TAOptimalTCFactorizationPass::runOnFunction()
     llvm::errs() << "Failed to applyPartialConversion in TAOptimalTCFactorizationPass\n";
     signalPassFailure();
   }
+}
+
+void SimulationAnalysisPass::runOnFunction()
+{
+  comet_debug() << " start SimulationAnalysisPass pass \n";
+  auto function = getFunction();
+
+  function.walk([&](tensorAlgebra::TensorMultOp op)
+              { SimulationAnalysis(op); });
+
+  comet_debug() << " end SimulationAnalysisPass pass \n";  
 }
 
 void LowerTAMulChainPass::runOnFunction()
@@ -218,4 +454,10 @@ std::unique_ptr<Pass> mlir::tensorAlgebra::createLowerTAMulChainPass()
 std::unique_ptr<Pass> mlir::tensorAlgebra::createSTCRemoveDeadOpsPass()
 {
   return std::make_unique<STCRemoveDeadOpsPass>();
+}
+
+// This pass enables hw/sw co-design. 
+std::unique_ptr<Pass> mlir::tensorAlgebra::createSimulationAnalysisPass()
+{
+  return std::make_unique<SimulationAnalysisPass>();
 }
