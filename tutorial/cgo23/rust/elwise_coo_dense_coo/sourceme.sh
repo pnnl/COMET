af5b00ce88fd028210ccb8733519e92ea34fbb01
Ryan D. Friese <ryan.friese@pnnl.gov>
2023-02-25 13:09:11 -0800

    adding sourceme file to rust tutorial

---
+++ tutorial/cgo23/rust/elwise_coo_dense_coo/sourceme.sh
@@ -0,0 +1,6 @@
+#/bin/sh
+
+export COMET_BIN_DIR=../../../../build/bin       #${COMET_ROOT}/build/bin
+export COMET_LIB_DIR=../../../../build/lib       #${COMET_ROOT}/build/lib
+export MLIR_BIN_DIR=../../../../llvm/build/bin   #${COMET_ROOT}/llvm/bin
+export MLIR_LIB_DIR=../../../../llvm/build/lib   #${COMET_ROOT}/llvm/lib
