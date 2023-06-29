b3ccc282de332767700a89c9fb00ff147b2b4bdd
Patrick Flynn <pflynn157@gmail.com>
2023-06-29 13:57:42 -0700

    Added simple script for running and testing things

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export SPARSE_FILE_NAME1=first.mtx
+
+#build/bin/comet-opt --convert-ta-to-it --convert-to-loops first.ta &> first.mlir
+#build/bin/comet-opt --convert-ta-to-it first.ta &> first.mlir
+
+build/bin/comet-opt --convert-ta-to-it --convert-to-loops --convert-to-llvm first.ta &> first.mlir
+
+llvm/build/bin/mlir-cpu-runner first.mlir -O3 -e main -entry-point-result=void \
+	-shared-libs=build/lib/libcomet_runner_utils.dylib,llvm/build/lib/libmlir_runner_utils.dylib
+
