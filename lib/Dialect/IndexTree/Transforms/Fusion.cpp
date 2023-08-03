c644850db032337ef14811b8095fdb47451f4977
rizwan.ashraf <rizwan.ashraf@knights.ucf.edu>
2023-08-03 13:12:21 -0700

    [masking] WIP: migrated masking codegen and added attribute

--- lib/Dialect/IndexTree/Transforms/Fusion.cpp
+++ lib/Dialect/IndexTree/Transforms/Fusion.cpp
@@ -919,6 +919,7 @@ void IndexTreeKernelFusionPass::insertTensorReset(
   //  auto comp_worksp_opt = builder.getBoolAttr(true);
   auto comp_worksp_opt = builder.getBoolAttr(false);
   mlir::StringAttr semiring = builder.getStringAttr("noop_times");
+  mlir::StringAttr maskType = builder.getStringAttr("none");
 
   // TODO(zpeng): here is I64 not F64? But it is F64 for ComputeRHS and ComputeLHS?
   IntegerType i64Type = IntegerType::get(builder.getContext(), 64);
@@ -928,7 +929,8 @@ void IndexTreeKernelFusionPass::insertTensorReset(
       compute_rhs,
       compute_lhs,
       comp_worksp_opt,
-      semiring);
+      semiring,
+      maskType);
   { // test
     comet_debug() << "compute_op\n";
     comet_vdump(compute_op);
