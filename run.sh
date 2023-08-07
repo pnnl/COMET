3663a4a542c6d31075ff79457f58604485d2df5c
Patrick Flynn <pflynn157@gmail.com>
2023-08-07 11:48:01 -0700

    Added ELLPACK print and SPMV tests

--- run.sh
+++ run.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # 2D
-export SPARSE_FILE_NAME0=integration_test/data/test_rank3.tns
-export SPARSE_FILE_NAME1=integration_test/data/test_rank3.tns
-export SPARSE_FILE_NAME=integration_test/data/test_rank3.tns
-export SORT_TYPE=SEQ_QSORT
+export SPARSE_FILE_NAME0=integration_test/data/test_rank2.mtx
+export SPARSE_FILE_NAME1=integration_test/data/test_rank2.mtx
+export SPARSE_FILE_NAME=integration_test/data/test_rank2.mtx
+#export SORT_TYPE=SEQ_QSORT
 
 #build/bin/comet-opt --convert-ta-to-it --convert-to-loops first.ta &> first.mlir
 #build/bin/comet-opt --convert-ta-to-it first.ta &> first.mlir
 
-build/bin/comet-opt --convert-to-loops --convert-to-llvm first.ta &> first.mlir
+build/bin/comet-opt  --convert-ta-to-it --convert-to-loops --convert-to-llvm first.ta &> first.mlir
 
 llvm/build/bin/mlir-cpu-runner first.mlir -O3 -e main -entry-point-result=void \
 	-shared-libs=build/lib/libcomet_runner_utils.dylib,llvm/build/lib/libmlir_runner_utils.dylib
