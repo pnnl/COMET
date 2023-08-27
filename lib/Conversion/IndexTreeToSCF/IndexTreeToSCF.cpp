9766a41c629aeaedc15cd7b1d3e85f2dda8a4898
Patrick Flynn <pflynn157@gmail.com>
2023-08-27 10:35:35 -0700

    Various fixes in IndexToSCF to clean up various hard-coded values and other temporary fixes

--- lib/Conversion/IndexTreeToSCF/IndexTreeToSCF.cpp
+++ lib/Conversion/IndexTreeToSCF/IndexTreeToSCF.cpp
@@ -2146,7 +2146,6 @@ void getAncestorsOps(OpsTree *opstree, std::vector<OpsTree *> &ret) {
 
 /// Generate scf.for op for indices
 /// The index is the "idx"th index of "tensor"
-// TODO: CHECK OPS HERE
 void genForOps(std::vector<Value> &tensors,
                std::vector<unsigned int> &ids,
                std::vector<std::string> &formats,
@@ -3955,7 +3954,7 @@ void genNewSparseTensorToPrint(OpBuilder &builder,
                   mtxC.getDefiningOp()->getOperands().end());
   operands[5] = mtxC_col_buffer;  // 3 (A2crd)
   operands[8] = mtxC_val_buffer;  // 4 (AVal)
-  SmallVector<Type, 12> elementTypes;
+  SmallVector<Type, 20> elementTypes;
   for (Value &opd : operands) {
     elementTypes.push_back(opd.getType());
   }
