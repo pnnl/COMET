a1457a0fae5a54bdb48040ac33a5712f9730f469
Polykarpos Thomadakis <polykarpos.thomadakis@pnnl.gov>
2024-01-04 16:32:45 -0800

    Changed setup.py to also look on default paths for comet and llvm if they are not explicitly set.

--- frontends/numpy-scipy/setup.py
+++ frontends/numpy-scipy/setup.py
@@ -3,10 +3,27 @@ import os
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
-if "COMETPY_COMET_PATH" in os.environ and "COMETPY_LLVM_PATH" in os.environ:
-    with open("cometpy/cfg.py", "w+") as cfg:
+with open("cometpy/cfg.py", "w+") as cfg:
+    if "COMETPY_COMET_PATH" in os.environ:
         cfg.write("comet_path = '{}'\n".format(os.environ["COMETPY_COMET_PATH"]))
+    else:
+        print("COMETPY_COMET_PATH not specified attempting default path")
+        if os.path.exists(os.path.abspath("../../build/")):
+            cfg.write("comet_path = '{}'\n".format(os.path.abspath("../../build/")))
+        else:
+            print("ERROR! Path to COMET not found.")
+            exit(0)
+
+    if "COMETPY_LLVM_PATH" in os.environ:
         cfg.write("llvm_path = '{}'\n".format(os.environ["COMETPY_LLVM_PATH"]))
+    else:
+        print("COMETPY_LLVM_PATH not specified attempting default path")
+        if os.path.exists(os.path.abspath("../../llvm/build/")):
+            cfg.write("llvm_path = '{}'\n".format(os.path.abspath("../../llvm/build/")))
+        else:
+            print("ERROR! Path to COMET LLVM not found.")
+            exit(0)
+
 
 setuptools.setup(
     name="cometpy",
