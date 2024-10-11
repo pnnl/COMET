b9d31790fdbf47b189201252ad28a5eea1b435f8
Polykarpos <polykarpos.thomadakis@pnnl.gov>
2024-10-11 11:42:09 -0700

    1. Added the ability to lower GPU code to either binary (cubin) or assembly (ptx) format. Theoretically, the binary format should save some time since the code does not need to be JIT compiled by the cuda driver at runtime. Assembly format is also provided to allow lowering even if there is not cuda runtime available
    2. The cuLaunch wrapper no longer needs to copy the kernel name to insert the \0
    character. The extra \0 character is now present in the generated global string
    
    3. Fixed bug in CMakeLists where integration tests for gpu would never run

--- include/comet/Conversion/TritonToCuda/TritonToCudaPass.h
+++ include/comet/Conversion/TritonToCuda/TritonToCudaPass.h
@@ -2,6 +2,7 @@
 #define TRITON_CONVERSION_TRITONTOCUDA_H
 
 #include <memory>
+#include "comet/Dialect/Utils/Utils.h"
 
 namespace mlir {
 
@@ -17,7 +18,8 @@ std::unique_ptr<OperationPass<mlir::ModuleOp>> createLowerTritonDeviceToCudaPass
                                                 int threadsPerWarp,
                                                 int numCTAs,
                                                 int numStages,
-                                                int computeCapability);
+                                                int computeCapability,
+                                                mlir::tensorAlgebra::GPUCompilationFormat format);
 
 } // namespace triton
 } // namespace mlir
